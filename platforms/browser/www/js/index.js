60210d901c295e0124cce965fc0cb67d310ea182
Victor <victorlogos1993@gmail.com>
2018-03-16 16:55:38 +0700

    test

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,33 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady(){
+    console.log('Device Ready');
+}
+
+window.onload = function openBrowser() {
+   var url = 'https://www.cambomarket.com/en/home/';
+   var target = '_self';
+   var options = "location = yes"
+   var ref = cordova.InAppBrowser.open(url, target, options);
+   
+   ref.addEventListener('loadstart', loadstartCallback);
+   ref.addEventListener('loadstop', loadstopCallback);
+   ref.addEventListener('loadloaderror', loaderrorCallback);
+   ref.addEventListener('exit', exitCallback);
+
+   function loadstartCallback(event) {
+      console.log('Loading started: '  + event.url)
+   }
+
+   function loadstopCallback(event) {
+      console.log('Loading finished: ' + event.url)
+   }
+
+   function loaderrorCallback(error) {
+      console.log('Loading error: ' + error.message)
+   }
+
+   function exitCallback() {
+      console.log('Browser is closed...')
+   }
+}
